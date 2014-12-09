ea03bc450d4d35961fb75ded871fe1d31e377d8f
Izhack <af-faize@live.fr>
2014-12-10 00:34:49 +0100

    Mise en place de la gestion des erreurs

--- ConversionChiffreArabeEnEgyptien/FormConversion.cs
+++ ConversionChiffreArabeEnEgyptien/FormConversion.cs
@@ -8,70 +8,40 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace ConversionChiffreArabeEnEgyptien
-{
+{  
     public partial class FormConversion : Form
     {
         public FormConversion()
         {
             InitializeComponent();
         }
-        
-        private void btnValider_Click(object sender, EventArgs e)
-        {
-            /*r//Déclaration des variables
-            int nombre = Convert.ToInt32(tbNombre.Text);
-            //int nombre = 1234567;
-            string r = "";
-            int resulat;
-            string images = "";
-
-            //----------------------------
-            
-            //Opération de recuperation des nombres
-
-
-            for (int i = 1000000; i >= 1; i = i / 10)
-            {
-                resulat = nombre / i;
-                nombre = nombre % i;
-
-                switch (i)
-                {
-                    case 1000000: images = "Homme(s)";
-                        break;
-                    case 100000: images = "Grenouille(s)";
-                        break;
-                    case 10000: images = "Doight(s)";
-                        break;
-                    case 1000: images = "Fleure(s)";
-                        break;
-                    case 100: images = "Spirale(s)";
-                        break;
-                    case 10: images = "Pont(s)";
-                        break;
-                    case 1: images = "Barre(s)";
-                        break;
-                }
-
-
-                r = r + "Il y 'a " + resulat + " " + images +" \r\n";
-            }
-
-
-
-            //Affichage du resultat
-            //r = "Il y 'a " + nb1000000 + " hommes \r\n" + nb100000 + "crapeau";
-            tbResultat.Text = r; */
-        }
-
         private void tbNombre_TextChanged(object sender, EventArgs e)
         {
 
             if (tbNombre.Text != null)
             {
-                if (tbNombre.Text == "") { tbNombre.Clear(); }
+                if (tbNombre.Text == "") { tbNombre.Clear(); } //Vide la textbox
+
+                //Fonction magique 
+
+                int nombre = 0;
                 //Déclaration des variables
-                int nombre = Convert.ToInt32(tbNombre.Text);
+                if (tbNombre.Text == "")
+                {
+                    nombre = 0;
+                }
+                else
+                {
+                    tbNombre.MaxLength = 9;
+                    try
+                    {
+                        nombre = Convert.ToInt32(tbNombre.Text);
+
+                    }
+                    catch (Exception ex) {
+                        MessageBox.Show(ex.Message); 
+                    }
+                }
                 string r = "";
                 int resulat;
                 string images = "";
@@ -103,9 +73,7 @@ namespace ConversionChiffreArabeEnEgyptien
                         case 1: images = "Barre(s)";
                             break;
                     }
-
-
-                    r = r + "Il y 'a " + resulat + " " + images + " \r\n";
+                     r = r + "Il y 'a " + resulat + " " + images + " \r\n";
                 }
                 tbResultat.Text = r;
             }
